cfd1b29d976014d3608badb99283de1fccb352bc
ajitk47 <ajit.k764@gmail.com>
2018-07-08 23:37:32 +0530

    Add files via upload

---
+++ BasketCost/src/com/ajit/Basket.java
@@ -0,0 +1,16 @@
+package com.ajit;
+
+import java.util.List;
+
+public class Basket {
+	
+	List<Item> list;
+
+	public List<Item> getList() {
+		return list;
+	}
+
+	public void setList(List<Item> list) {
+		this.list = list;
+	}
+}
---
+++ BasketCost/src/com/ajit/CalculateTotalBasketCost.java
@@ -0,0 +1,15 @@
+package com.ajit;
+
+public class CalculateTotalBasketCost {
+	public static double getTotal(Basket basket) {
+		int total = 0;
+		if(basket != null ) {
+			for(Item item : basket.getList()) {
+				int unitPrice = item.getUnitPrice();
+				int quantity = item.getQuantity();
+				total = total + unitPrice * quantity;
+			}
+		}
+		return total;
+	}
+}
